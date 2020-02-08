25bab16f0596f032c9d31296c96f88e3de2314d3
johndougherty68 <johndougherty68@gmail.com>
2020-02-08 14:46:36 -0500

    Created HelloPrinter

---
+++ helloPrinter/src/helloPrinter/HelloPrinter.java
@@ -0,0 +1,10 @@
+package helloPrinter;
+
+public class HelloPrinter {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Class");
+	}
+
+}
